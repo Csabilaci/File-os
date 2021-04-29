7bd3c2a485bc6fc607a06350fd8069d8eca15e7a
Vlad <vlad@localhost.localdomain>
2021-04-29 11:39:09 +0200

    Passed

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,22 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    void exportPhonebook(Map<String, String> contacts, String output) {
+        if(contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        File result = new File(output);
+        try (FileWriter fileWriter = new FileWriter(result,true)){
+            for (Map.Entry<String,String> input : contacts.entrySet()) {
+                fileWriter.write(input.getKey()+": "+input.getValue() + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
